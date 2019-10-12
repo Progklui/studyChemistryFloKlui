1d1a26e7436353b269efa8b0b8423ff216c7ce8b
ProgKlui <florian.kluibenschedl@live.de>
2019-10-12 21:42:29 +0200

    Added first exercise in thermodynamics

--- Semester3/AnalytischesGrundpraktikum/Aufgabe2/SimulationTitrationskurve.R
+++ Semester3/AnalytischesGrundpraktikum/Aufgabe2/SimulationTitrationskurve.R
@@ -5,7 +5,7 @@ pk_w = 14
 pka_1 = 6.35
 pka_2 = 10.33
 
-conc_base = 0.04502 # in mol/l
+conc_base = 0.04508 # in mol/l
 conc_acid = 0.05 # in mol/l
 titer_acid = 0.9799
 
@@ -25,7 +25,7 @@ intervall_2 = deriv$y1[(length(deriv$y1)/2):length(deriv$y1)]
 pos_eqp_2 = which(deriv$y1 == max(intervall_2))
 eqp_2 = deriv$x1[[pos_eqp_2]]
   
-plot(titration_Curve, , xaxs = "i", xlim = c(0, volume_burette), yaxs = "i", ylim = c(0, 14), panel.first = grid(), type = "l", col = "red", lwd = 2, main="Acidimetrische Titration con Carbonat", xlab = "V(HCl) in ml", ylab = "pH")
+plot(titration_Curve, , xaxs = "i", xlim = c(0, volume_burette), yaxs = "i", ylim = c(0, 14), panel.first = grid(), type = "l", col = "red", lwd = 2, main="Acidimetrische Titration von Carbonat", xlab = "V(HCl) in ml", ylab = "pH")
 lines(deriv, type = "l", col = "blue", lwd = 1)
 abline(v = eqp_2, col = "red", lty = 2)
 abline(v = eqp_1, col = "red", lty = 2)
